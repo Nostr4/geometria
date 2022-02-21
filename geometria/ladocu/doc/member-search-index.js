083a4be08336c1d011e66674f175f3d11d0d040a
JuanMa <Nostr4_88@hotmail.com>
2022-02-21 12:36:53 +0100

    Documentar es de sabios, aun que leerla sea pesado

---
+++ geometria/ladocu/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"CirculoJVM","l":"area()"},{"p":"<Unnamed>","c":"CirculoJVM","l":"CirculoJVM(double, String)","u":"%3Cinit%3E(double,java.lang.String)"},{"p":"<Unnamed>","c":"CirculoJVM","l":"perimetro()"}];updateSearchResults();
