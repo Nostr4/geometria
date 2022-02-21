5f3b2083e718d6801bcd902cb63fc3f1888dd08e
JuanMa <Nostr4_88@hotmail.com>
2022-02-21 12:26:45 +0100

    Versión Comentanda

--- geometria/src/RectanguloJVM.java
+++ geometria/src/RectanguloJVM.java
@@ -1,8 +1,15 @@
-
+/**
+ * 
+ * @author Juan Manuel Vázquez
+ * Versión 1.2
+ */
 public class RectanguloJVM extends FiguraGeometricaJVM {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * 
+	 * @param Pide el tipo de figura y dos lados.  
+	 */
 	public RectanguloJVM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
@@ -10,11 +17,17 @@ public class RectanguloJVM extends FiguraGeometricaJVM {
 	}
 
 	@Override
+	/**
+	 * Calcula el area.
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/**
+	 * Calcula el perímetro.
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
